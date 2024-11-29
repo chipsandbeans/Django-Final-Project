72ffb91f015ddeb0c6bf6a98cf142f01a3a41fe0
Byran Murphy <bryanmurphy401@gmail.com>
2024-11-29 16:45:45 +0000

    coded and migrated the Meal model

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Meal(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    protein = models.PositiveIntegerField()
+    carbs = models.PositiveIntegerField()
+    fats = models.PositiveIntegerField()
+    # Timestamp automatically saves time and date. 
+    meal_time = models.DateTimeField(auto_now_add=True)
+
+    def total_calories(self):
+        return (self.protein *4) + (self.carbs * 4) + (self.fats * 9)
