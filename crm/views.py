0864edbb9dc808d6b2e6ab07f35321cbb3663ec8
Byran Murphy <bryanmurphy401@gmail.com>
2024-12-02 22:32:23 +0000

    added previous button to the page pagination

--- crm/views.py
+++ crm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Meal
 
+"""
 def meal_list(request):
     # Fetch meals that belong to the current logged-in user
     meals = Meal.objects.filter(user=request.user)
@@ -9,3 +10,8 @@ def meal_list(request):
     paginate_by = 6
     
     return render(request, 'meal_list.html', {'object_list': meals})
+"""
+
+def meal_list(request):
+    meals = Meal.objects.all()  # Retrieve all meals
+    return render(request, 'meal_list.html', {'meals': meals})
