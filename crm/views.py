320caa8106b2c2476c5f7229a7a27a49a4a85aab
Byran Murphy <bryanmurphy401@gmail.com>
2024-12-05 11:40:40 +0000

    Using function based views I have made a 'create' function

--- crm/views.py
+++ crm/views.py
@@ -3,18 +3,23 @@ from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .models import Meal
 
-"""
-def meal_list(request):
-    # Fetch meals that belong to the current logged-in user
-    meals = Meal.objects.filter(user=request.user)
-    template_name = "crm/index.html"
-    paginate_by = 6
-    
-    return render(request, 'meal_list.html', {'object_list': meals})
-"""
 # Shows meals if logged in, if not redirects to login
 @login_required
 def meal_list(request):
     user_id = request.user.id 
     meals = Meal.objects.filter(user_id=user_id)
     return render(request, 'meal_list.html', {'meals': meals})
+
+# Create: Add a new meal 
+@login_required
+def meal_create(request):
+    if request.method == "POST":
+        form = MealForm(request.POST)
+        if form.is_valid():
+            meal = form.save(commit=False)
+            meal.user = request.user 
+            meal.save()
+            return redirect('meal-list')
+    else:
+        form = MealForm()
+    return render(request, 'crm/meal_form.html', {'form': form, 'action': 'Create'})
