b8c2b7f44ab83bd1ccec3fb7a65c6ec9bea670f5
Byran Murphy <bryanmurphy401@gmail.com>
2024-12-05 22:15:40 +0000

    created a basic model for weight tracking. It allows users to enter their weight in either kg or lbs.

--- crm/models.py
+++ crm/models.py
@@ -14,3 +14,20 @@ class Meal(models.Model):
 
     def total_calories(self):
         return (self.protein *4) + (self.carbs * 4) + (self.fats * 9)
+
+class WeightTracking(model.Models):
+    
+    UNIT_CHOICES = (
+        ('kg', 'Kilograms'),
+        ('lbs', 'Pounds'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    unit = models.CharField(max_length=3, choices=UNIT_CHOICES, default='kg')
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.weight} {self.unit}"
