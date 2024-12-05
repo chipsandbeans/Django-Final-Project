df5d41506fca5b333a8d758ab8012c8bd9cb3c25
Byran Murphy <bryanmurphy401@gmail.com>
2024-12-05 14:41:59 +0000

    Changed from function based to class based generic views. Created a meal form which allows users to create a meal

--- crm/urls.py
+++ crm/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from .views import (MealListView, MealDetailView, MealCreateView,)
 
 urlpatterns = [
     # path('meals/', views.meal_list, name='meal-list'),
-    path('', views.meal_list, name='home'),
+    path('', MealListView.as_view(), name='home'),
+    path('<int:pk>/', MealDetailView.as_view(), name='meal-detail'),
+    path('create/', MealCreateView.as_view(), name='meal-create'),
     ]
--- crm/views.py
+++ crm/views.py
@@ -1,25 +1,24 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Meal
 
-# Shows meals if logged in, if not redirects to login
-@login_required
-def meal_list(request):
-    user_id = request.user.id 
-    meals = Meal.objects.filter(user_id=user_id)
-    return render(request, 'meal_list.html', {'meals': meals})
+class MealListView(ListView):
+    model = Meal 
+    template_name = 'crm/meal_list.html'
+    context_object_name = 'meals'
 
-# Create: Add a new meal 
-@login_required
-def meal_create(request):
-    if request.method == "POST":
-        form = MealForm(request.POST)
-        if form.is_valid():
-            meal = form.save(commit=False)
-            meal.user = request.user 
-            meal.save()
-            return redirect('meal-list')
-    else:
-        form = MealForm()
-    return render(request, 'crm/meal_form.html', {'form': form, 'action': 'Create'})
+class MealDetailView(DetailView):
+    model = Meal
+    template_name = 'crm/meal_detail.html'
+    context_object_name = 'meal'
+
+class MealCreateView(CreateView):
+    model = Meal
+    template_name = 'crm/meal_form.html'
+    fields = ['title', 'protein', 'carbs', 'fats']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # Set the user as the logged-in user
+        return super().form_valid(form)
